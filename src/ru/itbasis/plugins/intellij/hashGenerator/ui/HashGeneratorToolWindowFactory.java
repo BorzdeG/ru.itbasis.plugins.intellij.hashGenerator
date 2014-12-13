8b31e2820c19b49a6bba52237231d9d4bc1be7dd
Victor Alenkov <Victor@Alenkov.name>
2014-12-13 14:36:11 +0300

    add annotations

--- src/ru/itbasis/plugins/intellij/hashGenerator/ui/HashGeneratorToolWindowFactory.java
+++ src/ru/itbasis/plugins/intellij/hashGenerator/ui/HashGeneratorToolWindowFactory.java
@@ -4,11 +4,13 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
+import org.jetbrains.annotations.NotNull;
 import ru.itbasis.plugins.intellij.hashGenerator.ui.forms.MainToolPanel;
 
 public class HashGeneratorToolWindowFactory implements ToolWindowFactory {
 	@Override
-	public void createToolWindowContent(Project project, com.intellij.openapi.wm.ToolWindow toolWindow) {
+	public void createToolWindowContent(@NotNull Project project,
+	                                    @NotNull com.intellij.openapi.wm.ToolWindow toolWindow) {
 
 		ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
 		Content content = contentFactory.createContent(new MainToolPanel().getMainPanel(), "", false);
